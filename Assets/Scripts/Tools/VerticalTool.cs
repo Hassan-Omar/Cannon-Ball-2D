95bea2b24369274c259a5cfd9afada614baf46bb
Hassan Omar <49106965+Hassan-Omar@users.noreply.github.com>
2020-05-22 18:19:08 +0200

    Add 3 Tool Controllers

---
+++ Assets/Scripts/Tools/VerticalTool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalTool : MonoBehaviour
+{
+    private bool moveFlag;
+    private int direction = -1;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        moveFlag = true;
+        StartCoroutine("InvertDirection");
+    }
+
+    private void FixedUpdate()
+    {
+        if (moveFlag)
+        {
+            moveDelta(0.03f, direction);
+        }
+    }
+
+    private void moveDelta(float delta, float direction)
+    {
+        transform.position += new Vector3(0, delta, 0) * Time.deltaTime * direction;
+    }
+
+    IEnumerator InvertDirection()
+    {
+        yield return new WaitForSeconds(2);
+        direction = 1;
+        yield return new WaitForSeconds(2);
+        moveFlag = false;
+    }
+}
